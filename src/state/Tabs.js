fa9a5e010a93966dcaace5629f37b5a71e85e248
Armin Husanovic <Armin@Armins-MacBook-Pro.local>
2019-07-17 06:09:30 -0400

    added tabs

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 class Tabs extends React.Component {
+  static defaultProps = { tabs: [] };
   render() {
     const button = this.props.tabs.map((tab, index) => (
       <button key={index}>{tab.name}</button>
     ));
-    return <div>{button}</div>;
+    const currentTab = this.props.tabs[0];
+    return <div className="content">{currentTab.content}</div>;
   }
 }
 
