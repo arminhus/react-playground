ff96e64f30b1619a52e5cb6720869953202f9bff
arminhus <husanovicarmin@gmail.com>
2019-07-18 23:06:37 -0400

    save

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -1,35 +1,74 @@
 import React from "react";
 
 class Tabs extends React.Component {
-  static defaultProps = { tabs: [] };
+  static defaultProps = {
+    tabs: []
+  };
 
   state = {
     currentTabIndex: 0
   };
 
   handleButtonClick = index => {
-    console.log("button clicked", { index });
-    this.setState({ currentTabIndex: index });
+    console.log(
+      "button clicked",
+      {
+        index
+      },
+      {
+        currentTabIndex: index
+      }
+    );
+    let number = index.target.getAttribute("data-index");
+    console.log(number);
+
+    this.setState({
+      currentTabIndex: number
+    });
   };
+  // undefined
 
   renderButton() {
     return this.props.tabs.map((tab, index) => (
-      <button key={index} onClick={this.handleButtonClick}>
-        {tab.name}
+      <button key={index} data-index={index} onClick={this.handleButtonClick}>
+        {" "}
+        {tab.name}{" "}
       </button>
     ));
   }
 
+  // renderButton alternative
+  // i take this approach
+  handleButtonClickAlt(index) {
+    console.log("alternative", index);
+    this.setState({ currentTabIndex: index });
+  }
+  renderButtonAlt() {
+    return this.props.tabs.map((tab, index) => (
+      <button key={index} onClick={() => this.handleButtonClickAlt(index)}>
+        {tab.name}{" "}
+      </button>
+    ));
+  }
+  // renderButton alternative
+
+  // BRB gotta go help someon
+
   renderContent() {
     const currentTab = this.props.tabs[this.state.currentTabIndex];
-    return <div className="content">{currentTab.content}</div>;
+    console.log(currentTab);
+    return <div className="content"> {currentTab.content} </div>;
   }
 
   render() {
     return (
       <div>
+        {" "}
         {this.renderButton()}
-        {this.props.tabs.length && this.renderContent()}
+        {/* renderButton alternative */}
+        {this.renderButtonAlt()}
+        {/* renderButton alternative */}
+        {this.props.tabs.length && this.renderContent()}{" "}
       </div>
     );
   }
