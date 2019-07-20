fc712d1dc1d9985abeb476aa687c81db04eb8dfb
AJ Makhl <ajmakhl@gmail.com>
2019-07-20 04:20:36 -0400

    Update Tabs.js

--- src/state-drills/Tabs.js
+++ src/state-drills/Tabs.js
@@ -2,17 +2,26 @@ import React from "react";
 
 class Tab extends React.Component {
   render() {
+    // thoughts
+    // here I just created a local active variable
+    // if the boolean that is passed here determines show or hide
+    // for the className of the content
+    let active = this.props.active ? "show" : "hide";
     return (
       <div className="tab">
         <div
           className="title flex"
-          onClick={() => {
-            this.props.handleclick(this.props.title);
-          }}
+          // thoughts
+          // this onclick can be simplified
+          onClick={this.props.handleclick}
         >
           <span>{this.props.title}</span>
         </div>
-        <div className={this.props.active ? "content show" : "content hide"}>
+        {/* thoughts */}
+        {/* I used template strings to  */}
+        {/* add the local active variable */}
+        {/* this helps with readability */}
+        <div className={`content ${active}`}>
           {this.props.content}
         </div>
       </div>
